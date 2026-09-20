122f43ae90a6a88f30aa971e16b0f0a155dd3b5b
Artem Cherepanov <artemcherepanov2357@gmail.com>
2026-09-20 05:43:35 +0300

    Add password fields to user update form, cleanup form labels

--- apps/users/forms.py
+++ apps/users/forms.py
@@ -16,6 +16,17 @@ class CustomUserCreationForm(UserCreationForm):
 
 
 class CustomUserUpdateForm(forms.ModelForm):
+    password1 = forms.CharField(
+        label=_('Password'),
+        widget=forms.PasswordInput,
+        required=False,
+    )
+    password2 = forms.CharField(
+        label=_('Password confirmation'),
+        widget=forms.PasswordInput,
+        required=False,
+    )
+
     class Meta:
         model = User
         fields = ('first_name', 'last_name', 'username')
@@ -24,6 +35,24 @@ class CustomUserUpdateForm(forms.ModelForm):
             'last_name': _('Last name'),
             'username': _('Username'),
         }
+
+    def clean(self):
+        cleaned_data = super().clean()
+        p1 = cleaned_data.get('password1')
+        p2 = cleaned_data.get('password2')
+        if p1 or p2:
+            if p1 != p2:
+                raise forms.ValidationError(_("Passwords don't match"))
+        return cleaned_data
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        password = self.cleaned_data.get('password1')
+        if password:
+            user.set_password(password)
+        if commit:
+            user.save()
+        return user
 
 class CustomAuthenticationForm(AuthenticationForm):
     username = forms.CharField(
