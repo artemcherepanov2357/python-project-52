462e3ec8d6a58ee710b32e75aa0622584ac4e622
Artem Cherepanov <artemcherepanov2357@gmail.com>
2026-09-20 07:04:53 +0300

    Show full name in executor dropdown instead of username

--- apps/tasks/forms.py
+++ apps/tasks/forms.py
@@ -1,10 +1,22 @@
 from django import forms
+from django.contrib.auth.models import User
 from django.utils.translation import gettext_lazy as _
 
 from .models import Task
 
 
+class UserChoiceField(forms.ModelChoiceField):
+    def label_from_instance(self, user):
+        return user.get_full_name() or user.username
+
+
 class TaskForm(forms.ModelForm):
+    executor = UserChoiceField(
+        queryset=User.objects.all(),
+        required=False,
+        label=_('Executor'),
+    )
+
     class Meta:
         model = Task
         fields = ('name', 'description', 'status', 'executor', 'labels')
